9b8735536339a0027a79041b8b551c3e8df9ff23
Xhanti <xhantimpu@gmail.com>
2022-06-26 11:48:53 +0200

    Last

--- index.js
+++ index.js
@@ -1,46 +1,59 @@
 // Create a new list item when clicking on the "Add" button
+const todos = []
+// const toDo = [
+//     {name: ${li}}
+// ]
 
-const toDo = [
-    {name: ${li}}
-]
-
-let items = JSON.parse(localStorage.getItem("items")) || [];
+// let items = JSON.parse(localStorage.getItem("items")) || [];
 function newTask() {
-  let li = document.createElement("label");
+  let eachTask = document.createElement("div");
+  eachTask.classList.add("each_task")
+  let li = document.createElement("input");
   li.classList.add("taskList");
   li.type = "text";
   let newTask = document.getElementById("newTask").value;
-  let task = document.createTextNode(newTask);
-  li.appendChild(task);
+  li.value = newTask;
+  li.setAttribute('readonly', 'readonly');
+  let taskObject = {
+    title: newTask,
+    createdDate: new Date(),
+    id: todos.length +1,
+    completed: false
+  }
+  todos.push(taskObject)
+  console.log(todos);
+  // let task = document.createTextNode(newTask);
+  // li.appendChild(task);
   if (newTask === "") {
     alert("Insert task!");
   } else {
     document.getElementById("to_do_items").appendChild(li);
   }
   document.getElementById("newTask").value = "";
-
+  
   const taskActions = document.createElement("div");
   taskActions.classList.add("actions");
-
+  
   const taskEdit = document.createElement("button");
   taskEdit.classList.add("edit");
   taskEdit.innerText = "Edit";
-
+  
   const taskDelete = document.createElement("button");
   taskDelete.classList.add("delete");
   taskDelete.innerText = "Delete";
-
+  
   taskActions.appendChild(taskEdit);
   taskActions.appendChild(taskDelete);
-
-  li.appendChild(taskActions);
-  document.getElementById("to_do_items").appendChild(li);
+  
+  eachTask.appendChild(li);
+  eachTask.appendChild(taskActions);
+  document.getElementById("to_do_items").appendChild(eachTask);
 
   taskEdit.addEventListener("click", (e) => {
     if (taskEdit.innerText.toLowerCase() == "edit") {
       taskEdit.innerText = "Save";
-      task_input_el.removeAttribute("readonly");
-      task_input_el.focus();
+      li.removeAttribute("readonly");
+      li.focus();
     } else {
       taskEdit.innerText = "Edit";
       taskEdit.setAttribute("readonly", "readonly");
